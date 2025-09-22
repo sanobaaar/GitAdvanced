de1b49e1a89a63520ee7af12dd085f9050776b7f
SanobarEnigma <sanobarfatima96@gmail.com>
2025-09-22 03:28:47 -0700

    rebase commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,8 +16,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
 
-        System.out.println("Hello world!");
-
 
         //method call
         decrement(num);
