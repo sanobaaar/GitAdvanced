a3057d24a1e42020efe7f4f7b6496eafe9f3de3b
SanobarEnigma <sanobarfatima96@gmail.com>
2025-09-22 00:40:08 -0700

    created main

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,41 @@
+package com.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int sum = add(12, 3);
+        System.out.println(sum);
+
+      
+        int num = 5;
+        //method call
+        increment(num);
+        System.out.println("Outside function: " + num);
+
+        int[] myArray = { 1, 2, 3 };
+        updateArray(myArray);
+        System.out.println("First element: " + myArray[0]);
+
+        String[] names = greetUsers();
+        System.out.println("First name: " + names[0]);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void increment(int number) {
+        number++;
+        System.out.println("Inside function: " + number);
+    }
+
+    public static void updateArray(int[] numbers) {
+        numbers[0] = 100;
+    }
+
+    public static String[] greetUsers() {
+        return new String[] { "ABC", "XYZ" };
+    }
+
+}
